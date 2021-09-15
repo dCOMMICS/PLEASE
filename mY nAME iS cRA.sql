ec1d809b59cdd2bf9cab3714522f66969f7a2a5b
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2021-09-15 14:42:33 +0300

    Add files via upload

---
+++ mY nAME iS cRA.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE JUHI;
+USE JUHI;
+
+/*CREATE TABLE Employee
+(EmployeeID int,
+EmployeeName varchar(50),
+EmployeeSalary int,
+EmployeeDepartment varchar(50),
+EmployeeAge int,
+Location varchar(50),
+Tel int
+)*/
+
+/*INSERT INTO Employee VALUES
+(1245,'Monica',45000,'Human Resource',25,'Mtwapa',074578256)
+*/
+INSERT INTO Employee VALUES
+(1246,'Vincent',30000,'Human Resource',20,'Moyale',07256401)
+   
+
+
